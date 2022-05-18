04f24396a78c4e376c4dc8b0d65d3e0e0fd29d3f
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-05-18 02:47:15 -0400

    Job Trackr App fully Functioning w/ updates

--- script.js
+++ script.js
@@ -17,9 +17,12 @@ function formSubmit(){
 function readFormInput(){
     var formInfo = {};
     formInfo["companyName"] = document.getElementById("companyName").value;
+    formInfo["positionTitle"] = document.getElementById("positionTitle").value;
     formInfo["dateApplied"] = document.getElementById("dateApplied").value;
+    formInfo["applyMethod"] = document.getElementById("applyMethod").value;
     formInfo["salary"] = document.getElementById("salary").value;
     formInfo["cityState"] = document.getElementById("cityState").value;
+    formInfo["appStatus"] = document.getElementById("appStatus").value;
     return formInfo;
 }
 
@@ -31,22 +34,31 @@ function inputNewRecord(data) {
    cell1 = newRow.insertCell(0); // call function insert cell with the index of the cell
    cell1.innerHTML = data.companyName;
    cell2 = newRow.insertCell(1); // get the index of the cell
+   cell2.innerHTML = data.positionTitle;
+   cell2 = newRow.insertCell(2);
    cell2.innerHTML = data.dateApplied;
-   cell3 = newRow.insertCell(2);
-   cell3.innerHTML = data.salary;
-   cell4 = newRow.insertCell(3);
-   cell4.innerHTML = data.cityState;
+   cell3 = newRow.insertCell(3);
+   cell3.innerHTML = data.applyMethod;
    cell4 = newRow.insertCell(4);
-   cell4.innerHTML = `<a onClick="editBtn(this)">Edit</a>
+   cell4.innerHTML = data.salary;
+   cell5 = newRow.insertCell(5); 
+   cell5.innerHTML = data.cityState;
+   cell6 = newRow.insertCell(6);
+   cell6.innerHTML = data.appStatus;
+   cell6 = newRow.insertCell(7);
+   cell6.innerHTML = `<a onClick="editBtn(this)">Edit</a>
                      <a onClick="onDelete(this)">Delete</a>`; //  u can use "" , '' or ``
 }
 
 // Reset the Form
 function resetForm(){
    document.getElementById("companyName").value = "";
+   document.getElementById("positionTitle").value = "";
    document.getElementById("dateApplied").value = "";
+   document.getElementById("applyMethod").value = "";
    document.getElementById("salary").value = "";
    document.getElementById("cityState").value = "";
+   document.getElementById("appStatus").value = "";
    rowPicker = null;
 }
 // Function to EDIT entries
@@ -54,17 +66,23 @@ function resetForm(){
 function editBtn(td) {
    rowPicker = td.parentElement.parentElement;
    document.getElementById("companyName").value = rowPicker.cells[0].innerHTML;
-   document.getElementById("dateApplied").value = rowPicker.cells[1].innerHTML;
-   document.getElementById("salary").value = rowPicker.cells[2].innerHTML;
-   document.getElementById("cityState").value = rowPicker.cells[3].innerHTML;
+   document.getElementById("positionTitle").value = rowPicker.cells[1].innerHTML;
+   document.getElementById("dateApplied").value = rowPicker.cells[2].innerHTML;
+   document.getElementById("applyMethod").value = rowPicker.cells[3].innerHTML;
+   document.getElementById("salary").value = rowPicker.cells[4].innerHTML;
+   document.getElementById("cityState").value = rowPicker.cells[5].innerHTML;
+   document.getElementById("appStatus").value = rowPicker.cells[6].innerHTML;
 }
 
 //  Update Entry
 function updateData(formInfo) {
    rowPicker.cells[0].innerHTML = formInfo.companyName;
-   rowPicker.cells[1].innerHTML = formInfo.dateApplied;
-   rowPicker.cells[2].innerHTML = formInfo.salary;
-   rowPicker.cells[3].innerHTML = formInfo.cityState;
+   rowPicker.cells[1].innerHTML = formInfo.positionTitle;
+   rowPicker.cells[2].innerHTML = formInfo.dateApplied;
+   rowPicker.cells[3].innerHTML = formInfo.applyMethod;
+   rowPicker.cells[4].innerHTML = formInfo.salary;
+   rowPicker.cells[5].innerHTML = formInfo.cityState;
+   rowPicker.cells[6].innerHTML = formInfo.appStatus;
 }
 
 // Delete an Entry
